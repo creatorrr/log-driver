51a5e270162853e84200357e8252dbf764f46b1e
Diwank Singh <diwank.singh@gmail.com>
2015-03-22 01:01:42 +0530

    Added driver refs to logger core
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import filter from "./drivers/filter";
 import format from "./drivers/format";
 import console from "./drivers/console";
 import restore from "./drivers/restore";
+import { assign } from "../utils";
 
 let logger = new Logger;
 
@@ -12,6 +13,11 @@ logger
   .pipe(format())
   .pipe(console)
   .pipe(restore);
+
+// Add basic drivers
+assign(Logger.drivers, {
+  filter, format, console, restore
+});
 
 export {
   logger, Logger
